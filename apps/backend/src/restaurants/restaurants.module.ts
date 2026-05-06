48e8df0f0a4e3e77cdec8121027356ce96e37014
Renova69 <124306518+Renova69@users.noreply.github.com>
2026-05-06 22:50:24 +0300

    feat: add Stripe Connect routes to RestaurantsController

--- apps/backend/src/restaurants/restaurants.module.ts
+++ apps/backend/src/restaurants/restaurants.module.ts
@@ -3,9 +3,10 @@ import { RestaurantsService } from './restaurants.service';
 import { RestaurantsController } from './restaurants.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { TranslationModule } from '../translation/translation.module';
+import { PaymentModule } from '../payment/payment.module';
 
 @Module({
-  imports: [PrismaModule, TranslationModule],
+  imports: [PrismaModule, TranslationModule, PaymentModule],
   controllers: [RestaurantsController],
   providers: [RestaurantsService],
   exports: [RestaurantsService],
